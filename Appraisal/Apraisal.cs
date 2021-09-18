f91d455d595e23e299cb66c99607b2332e22794e
JitendraBordiya <Jitendra7496@gmail.com>
2021-09-18 14:04:59 +0530

    Project File

---
+++ Appraisal/Apraisal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Appraisal
+{
+    public class Apraisal
+    {
+        public string cons;
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+        public static void CreateConnection()
+        {
+            string cons = "Data Source=LAPTOP-FS8H47HQ\\DBSERVER;Initial catalog=Appraisal;User Id=sa;password=@123";
+            con = new SqlConnection();
+            con.ConnectionString = cons;
+        }
+        public static void InsertData(string EmpId,string Name, string DeptNo, string CurrRole,string DeptName,string NewRole,int AppNo )
+        {
+            con.Open();
+            string query = "Insert Into Employee(EmpId,EmpName,DeptNo,DeptName,CurrentRole,NewRole,AppNo) values('" + EmpId + "','" + Name + "','" + DeptNo + "','" + DeptName + "','" + CurrRole+ "','" + NewRole + "','" + AppNo + "')";
+
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} Record Insert Suceesfully", r);
+            con.Close();
+        }
+        public static void DisplayData()
+        {
+            con.Open();
+            string query = "Select * From Employee";
+
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine("{0} | {1} | {2} | {3} | {4} | {5}  |  {6} | {7} ",dr["EmpId"],dr["EmpName"], dr["DeptNo"],dr["DeptName"],dr["CurrentRole"], dr["NewRole"], dr["AppDate"], dr["AppNo"]);
+                
+            }
+            dr.Close();
+            string cmdCount = "Select Count(*) from Employee";
+            cmd = new SqlCommand(cmdCount, con);
+            int Count = (int)cmd.ExecuteScalar();
+            Console.WriteLine("{0} Records ", Count);
+            con.Close();
+        }
+        public static void HighAppData()
+        {
+            con.Open();
+            string query = "Select EmpId,EmpName,DeptName,CurrentRole From Employee Where AppNo=5";
+
+            cmd = new SqlCommand(query, con);
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("EmpID || Name   ||   DeptName  || CurrRole  ");
+            while (dr.Read())
+            {
+                Console.WriteLine("{0} \t   {1}   \t   {2}   \t   {3} ", dr["EmpId"],dr["EmpName"],dr["DeptName"],dr["CurrentRole"]);
+
+            }
+            dr.Close();
+            con.Close();
+        }
+
+
+        public static void UpdateData(string NewRole,string id ,int AppNo)      
+        {
+            con.Open();
+            string query = "update Employee Set CurrentRole='"+NewRole+"',NewRole='"+NewRole+ "', AppNo='" + AppNo + "'Where EmpId='" + id+"'";
+
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} Record Updates", r);
+            con.Close();
+        }
+        public static void DeleteData(string id)
+        {
+            con.Open();
+            string query = "Delete From Employee Where EmpId='" + id + "' ";
+
+            cmd = new SqlCommand(query, con);
+            int r = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} row Deleted", r);
+            con.Close();
+        }
+    }
+}
---
+++ AppraisalHistory/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.SqlClient;
+using Appraisal;
+
+namespace AppraisalHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            char Choice;
+            string Id;
+            string Name;
+            string DeptNo;
+            string DeptName;
+            string CurrRole;
+            string NewRole;
+            int AppNo;
+
+            Console.WriteLine("Confirm Your Validation");
+            //Console.Write("UserName :");
+            //string UserName = Console.ReadLine();
+            //Console.Write("Pass :");
+            //string PassWord = Console.ReadLine();
+
+
+
+     
+
+            Apraisal.CreateConnection();
+             Console.WriteLine("Connection SuccessFul");
+            Console.WriteLine("############################################################");
+
+            Console.WriteLine("          WelCome To T-Company    ");
+            Console.WriteLine("############################################################");
+
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1 to Check The Details Of Employee");
+            Console.WriteLine("2 to Add New Data");
+            Console.WriteLine("3 TO Update the Data");
+            Console.WriteLine("4 TO HighAppraisal Employee's  Data");
+            Console.WriteLine("5 Delete The Data");
+            Console.WriteLine("Press Y to Continue");
+            Console.WriteLine("_____________________________________________________________");
+
+            do
+            {
+                Console.WriteLine("_____________________________________________________________");
+
+
+                Console.WriteLine("Enter Your Choice");
+                i = Convert.ToInt32(Console.ReadLine());
+                switch (i)
+                    { 
+                   case 1:
+                        Apraisal.DisplayData();
+                        break;
+                    case 2:
+                       
+                        Console.WriteLine("Enter Your Details");
+                        Console.Write("Enter ID : ");
+                        Id = Console.ReadLine();
+                        Console.Write("Enter Name : ");
+                        Name = Console.ReadLine();
+                        Console.Write("Enter DeptID : ");
+                        DeptNo = Console.ReadLine();
+                        Console.Write("Enter DeptName : ");
+                        DeptName = Console.ReadLine();
+                        Console.Write("Enter Your Role : ");
+                        CurrRole = Console.ReadLine();
+                        Console.Write("Enter Your New Role : ");
+                        NewRole = Console.ReadLine();
+                        Console.Write("Enter No of Appraisal : ");
+                        AppNo = Convert.ToInt32(Console.ReadLine());
+                        Apraisal.InsertData(Id, Name, DeptNo, DeptName, CurrRole, NewRole, AppNo);
+                        break;
+                    case 3:
+                        Console.Write("Enter ID : ");
+                        Id = Console.ReadLine();
+                        Console.Write("Enter Your New Role : ");
+                        NewRole = Console.ReadLine();
+                        Console.Write("Enter Your AppNo : ");
+                        AppNo = Convert.ToInt32( Console.ReadLine());
+                        Apraisal.UpdateData( NewRole, Id,AppNo);
+                        break;
+                    case 4:
+                        Console.WriteLine("Employees List Who Are Intern Now Managers");
+                        Apraisal.HighAppData();
+                        break;
+                    case 5:
+                        Console.Write("Enter ID : ");
+                        Id = Console.ReadLine();
+
+                        Apraisal.DeleteData(Id);
+                        break;
+                }
+                 Console.Write("Want To revisit Data Press Y :");
+                 Choice = Convert.ToChar(Console.ReadLine());
+            } while(Choice == 'Y');
+
+            Console.Read();
+
+            
+        }
+    }
+}
